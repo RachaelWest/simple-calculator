6b7729aa6a59bf9aa3e3bf87241a2096128dee7f
rachcore1 <78730563+rachcore1@users.noreply.github.com>
2022-03-10 11:43:37 +0000

    amended html and js to remove maths option

--- script.js
+++ script.js
@@ -38,35 +38,9 @@ function calculate() {
             case 'divide':
                 result = divide(first_num, second_num)
                 break;
-            default:
-                result = "Not a valid math Symbol. Please try again.";
         }
         display.innerHTML = "Result : " + String(result);
     } else {
         display.innerHTML = "No mode was set";
     }
 }
-
-
-/*OLD VERSION
-function calculate() {
-    var first_num = Number(document.getElementById("first-num").value);
-    var second_num = Number(document.getElementById("second-num").value);
-    var result = 0;
-
-    if (document.getElementById("add").checked) {
-        result = first_num + second_num;
-    }
-    else if (document.getElementById("subtract").checked) {
-        result = first_num - second_num;
-    }
-    else if (document.getElementById("multiply").checked) {
-        result = first_num * second_num;
-    }
-    else if (document.getElementById("divide").checked) {
-        result = first_num / second_num;     
-    }
-
-    document.getElementById("result").innerHTML = "Result : " + String(result);
-}
-*/
